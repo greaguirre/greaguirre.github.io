46e52c7341f6f931649e40965d3025e2f63c7796
peppa-peddler <jorenmija@gmail.com>
2022-03-21 19:10:17 -0500

    adding design

---
+++ main.js
@@ -0,0 +1,87 @@
+function clear(){
+    $content = $('#content');
+    $content.html("");
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+function questions(data, id){
+    clear();
+
+    $content = $('#content');
+    $content.append("<div class='page'><div class='container'></div></div>");
+    $content = $content.find(".container");
+
+    $content.append("<p class='back'> <img src='images/back-arrow.png'> Go back</p>");
+    $content.append("<h2>"+data[ id ][ "title" ]+"<h2>")
+    $(".back").click(function(){index(data);})
+    problems = data[ id ][ "questions" ];
+    for( var i = 0; i < problems.length ; i++ ){
+	console.log(problems[i]);
+	$content.append(function(){
+	    sentence = "<p class='problem'>";
+	    parts = problems[ i ][ "parts" ];
+
+	    for( var j = 0; j < parts.length ; j++ ){
+		if( parts[ j ][ "type" ] == "text" )
+		    sentence += parts[ j ][ "value" ] + " ";
+		else
+		    sentence += '<input data-ans="' + parts[ j ][ "answer" ]+'"> ';
+	    }
+
+	    sentence += "<br>"
+	    sentence += "<span class='ans'><br></span>";
+	    sentence += "<br>"
+	    sentence += "<span class='check boton'>Check</span>";
+	    sentence += "<span class='show boton'>Show</span>";
+
+	    sentence += "</p>";
+	    return sentence;
+	});
+    }
+
+	$(".check").click(function(){
+	    $(this).parent().find('input').each(function(indx){
+		if( $(this).val() == $(this).data('ans') ){
+		    $(this).removeClass('wrong');
+		    $(this).addClass('right');
+		} else {
+		    $(this).removeClass('right');
+		    $(this).addClass('wrong');
+		}
+	    });
+	});
+	$(".show").click(function(){
+	    console.log("xd");
+	    $ans = $(this).parent().find('.ans');
+	    $ans.html("<span>| </span>");
+	    $(this).parent().find('input').each(function(indx){
+		$ans.append($(this).data('ans')+"<span> | </span>");
+	    })
+	});
+}
+
+function index(data){
+    clear();
+    $content = $('#content');
+    $content.append("<div class='container'></div>");
+    $content = $content.find(".container")
+    console.log(data.length);
+    category = "";
+    for( var i = 0; i < data.length ; i++ ){
+	if( category != data[ i ][ "category" ] ){
+	    category = data[ i ][ "category" ];
+	    $content.append( "<h2> <img src='images/quest.png'>" + category + "</h2>" );
+	}
+	$content.append( "<p class='question' data-id="+i+"><img src='images/right-arrow.png'> " + data[ i ]["title"] + "</p>" );
+    }
+    $(".question").click(function(){
+	id = questions(data, $(this).data("id"));
+    });
+}
+
+$(window).on('load', function(){
+    $.getJSON('questions.json', function(jd){
+	data = JSON.parse(jd);
+	index(data);
+    });
+});
