2b7226e169fe518934b2ac21ed27f2089b1269fd
peppa-peddler <jorenmija@gmail.com>
2022-03-23 21:10:50 -0500

    Adding questionary page instead of using javascript for that

--- main.js
+++ main.js
@@ -4,60 +4,7 @@ function clear(){
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
-function questions(data, id){
-    clear();
-    $content = $('#content');
-    $content.append("<div class='page'><div class='container'></div></div>");
-    $content = $content.find(".container");
-    $content.append("<p class='back'> <img src='images/back-arrow.png'> Go back</p>");
-    $content.append("<h2 class='titlepage'>"+data[ id ][ "title" ]+"</h2>")
-    $content.append("<h3>"+data[ id ][ "description" ]+"</h3>")
-    $(".back").click(function(){index(data);})
-    problems = data[ id ][ "questions" ];
-    for( var i = 0; i < problems.length ; i++ ){
-	$content.append(function(){
-	    sentence = "<p class='problem'><b>" + (i+1) + ")</b> ";
-	    parts = problems[ i ][ "parts" ];
 
-	    for( var j = 0; j < parts.length ; j++ ){
-		if( parts[ j ][ "type" ] == "text" )
-		    sentence += parts[ j ][ "value" ] + " ";
-		else
-		    sentence += '<input data-ans="' + parts[ j ][ "answer" ]+'"> ';
-	    }
-
-	    sentence += "<span class='botoncase'>";
-	    sentence += "<span class='check boton'>Check</span>";
-	    sentence += "<span class='show boton'>Show</span>";
-	    sentence += "</span>";
-	    sentence += "<br>";
-	    sentence += "<span class='ans'><br></span>";
-	    sentence += "<br>";
-
-	    sentence += "</p>";
-	    return sentence;
-	});
-    }
-
-    $(".check").click(function(){
-	$(this).parent().parent().find('input').each(function(indx){
-	    if( $(this).val() == $(this).data('ans') ){
-		$(this).removeClass('wrong');
-		$(this).addClass('right');
-	    } else {
-		$(this).removeClass('right');
-		$(this).addClass('wrong');
-	    }
-	});
-    });
-    $(".show").click(function(){
-	$ans = $(this).parent().parent().find('.ans');
-	$ans.html("<span>| </span>");
-	$(this).parent().parent().find('input').each(function(indx){
-	    $ans.append($(this).data('ans')+"<span> | </span>");
-	})
-    });
-}
 
 function index(data){
     clear();
@@ -70,12 +17,8 @@ function index(data){
 	    category = data[ i ][ "category" ];
 	    $content.append( "<h2> <img src='images/quest.png'>" + category + "</h2>" );
 	}
-	$content.append( "<p class='question' data-id="+i+"><img src='images/right-arrow.png'> " + data[ i ]["title"] + "</p>" );
+	$content.append( "<a class='question' href='questionary/?id="+i+"'><img src='images/right-arrow.png'> " + data[ i ]["title"] + "</a>" );
     }
-    $(".question").click(function(){
-	id = questions(data, $(this).data("id"));
-    });
-
 }
 
 $(window).on('load', function(){
---
+++ questionary/quest.js
@@ -0,0 +1,67 @@
+function questions(data, id){
+    $content = $('#content');
+    $content.append("<div class='page'><div class='container'></div></div>");
+    $content = $content.find(".container");
+    $content.append("<a href='/' class='back'> <img src='../images/back-arrow.png'> Go back</a>");
+    $content.append("<h2 class='titlepage'>"+data[ id ][ "title" ]+"</h2>")
+    $content.append("<h3>"+data[ id ][ "description" ]+"</h3>")
+    $(".back").click(function(){index(data);})
+    problems = data[ id ][ "questions" ];
+    for( var i = 0; i < problems.length ; i++ ){
+	$content.append(function(){
+	    sentence = "<p class='problem'><b>" + (i+1) + ")</b> ";
+	    parts = problems[ i ][ "parts" ];
+
+	    for( var j = 0; j < parts.length ; j++ ){
+		if( parts[ j ][ "type" ] == "text" )
+		    sentence += parts[ j ][ "value" ] + " ";
+		else
+		    sentence += '<input data-ans="' + parts[ j ][ "answer" ]+'"> ';
+	    }
+
+	    sentence += "<span class='botoncase'>";
+	    sentence += "<span class='check boton'>Check</span>";
+	    sentence += "<span class='show boton'>Show</span>";
+	    sentence += "</span>";
+	    sentence += "<br>";
+	    sentence += "<span class='ans'><br></span>";
+	    sentence += "<br>";
+
+	    sentence += "</p>";
+	    return sentence;
+	});
+    }
+
+    $(".check").click(function(){
+	$(this).parent().parent().find('input').each(function(indx){
+	    if( $(this).val() == $(this).data('ans') ){
+		$(this).removeClass('wrong');
+		$(this).addClass('right');
+	    } else {
+		$(this).removeClass('right');
+		$(this).addClass('wrong');
+	    }
+	});
+    });
+    $(".show").click(function(){
+	$ans = $(this).parent().parent().find('.ans');
+	$ans.html("<span>| </span>");
+	$(this).parent().parent().find('input').each(function(indx){
+	    $ans.append($(this).data('ans')+"<span> | </span>");
+	})
+    });
+}
+
+$(window).on('load', function(){
+    $.getJSON('../questions.json', function(jd){
+	data = JSON.parse(jd);
+
+	let searchParams = new URLSearchParams(window.location.search)
+	searchParams.has('id')
+	let id = searchParams.get('id')
+	console.log(id);
+
+	console.log( id || 0 );
+	questions(data, id || 0);
+    });
+});
